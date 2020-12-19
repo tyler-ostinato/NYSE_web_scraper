3f5741a0b3b7d235b77d810b148b79b87898c30a
Tyler Ostinato <tylerostinato@gmail.com>
2020-12-19 00:33:16 -0500

    Added duplication check and email forwarding

--- scraper.py
+++ scraper.py
@@ -2,26 +2,35 @@
 import requests
 import json
 import csv
-from utils import convert_epoch
+from utils import convert_epoch, hash_file, check_duplicate
 
 # Must use API becuse we cannot extract data through HTMl
 source = requests.get("https://www.nyse.com/api/ipo-center/calendar").text
 
-# open a CSV file to save the entries
-with open('IPO_Filings.csv', 'w', newline='') as file:
-    csv_output = csv.writer(file)
-    csv_output.writerow(["Date Filed", "Company", "Ticker", "Industry", "Price"])
+# Open a CSV file to save the entries
+csv_file = open('IPO_Filings.csv', 'w')
+csv_output = csv.writer(csv_file)
+csv_output.writerow(["Date Filed", "Company", "Ticker", "Industry", "Price"])
 
-    # Traverse JSON payload to get information
-    data_dict = json.loads(source)
-    companies_list = data_dict['calendarList']
-    for company in companies_list:
-        #Get useful information from the filings
-        issuer = company['issuer_nm']
-        ticker = company['symbol']
-        industry = company['custom_group_industry_nm']
-        price_range = company['current_file_price_range_usd']
-        date_filed = convert_epoch(company['init_file_dt']) # Change this to expected?
+# Traverse JSON payload to get information
+data_dict = json.loads(source)
+companies_list = data_dict['calendarList']
+for company in companies_list:
+    # Get useful information from the filings
+    issuer = company['issuer_nm']
+    ticker = company['symbol']
+    industry = company['custom_group_industry_nm']
+    price_range = company['current_file_price_range_usd']
+    date_filed = convert_epoch(company['init_file_dt']) # Change this to expected?
 
-        if company['deal_status_desc'] == 'Filed': # This will need to be 'Expected' I think
-            csv_output.writerow([date_filed, issuer, ticker, industry, price_range])
+    if company['deal_status_desc'] == 'Filed': # This will need to be 'Expected' I think
+        csv_output.writerow([date_filed, issuer, ticker, industry, price_range])
+
+# Close the file
+csv_file.close()
+
+# Call function to hash current file
+hash_number = hash_file()
+
+# Call function to check if this hash already exists
+check_duplicate(hash_number)
--- utils.py
+++ utils.py
@@ -1,7 +1,55 @@
 # File used to store functions for scraper.py
 import datetime
+import hashlib
+import smtplib, ssl
+from static import PASSWORD, PORT, sender_email, receiver_email, MESSAGE_BODY
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
 
 # Use to convert epoch time to human readable string
 def convert_epoch(time):
     time_string = datetime.datetime.fromtimestamp(time/1000).strftime('%c')
     return time_string
+
+
+def hash_file():
+    hasher = hashlib.md5() # Use MD5 Hashing
+    with open("IPO_Filings.csv", 'rb') as open_file:
+        content = open_file.read() # Read CSV contents
+        hasher.update(content)
+        hash_number = hasher.hexdigest() # Convert hash to hexidecimal output
+        return hash_number
+    open_file.close()
+
+
+def check_duplicate(hash_number):
+    stored_hashes = open("hash_data.txt", 'r+')
+
+    if hash_number in stored_hashes.read():
+        print("No new information found.")
+    else:
+        # Add the new hash number
+        stored_hashes.write(f'\n{hash_number}')
+        # Email the CSV file
+        print("New information was found! Sending email.")
+        send_email()
+    stored_hashes.close()
+
+
+def send_email():
+    msg = MIMEMultipart()
+    msg['Subject'] = "NYSE IPO Filings"
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    body = MIMEText(MESSAGE_BODY, 'plain')
+    msg.attach(body)
+
+    with open("IPO_Filings.csv", 'rb') as file:
+        msg.attach(MIMEApplication(file.read(), Name="IPO_Filings.csv"))
+    file.close()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", PORT) as smtp:
+        smtp.login(sender_email, PASSWORD)
+        smtp.sendmail(msg['From'], msg['To'], msg.as_string())
